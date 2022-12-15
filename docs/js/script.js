0298229ec1097dadc615397447af566e854bdf5b
Vlad <klock.233@yandex.ru>
2022-12-15 15:40:13 +0300

    prototype kinda ready 2try

---
+++ docs/js/script.js
@@ -0,0 +1,7 @@
+
+$(function(){
+    $('.item__carousel').slick({
+        prevArrow: "<img src='/img/previous-svgrepo-com.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='/img/next-svgrepo-com.svg' class='next' alt='2'>"
+    })
+})
