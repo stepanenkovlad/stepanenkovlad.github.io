f62b118c820cfe193d10fd418ef0685416f9f8e7
Vlad <klock.233@yandex.ru>
2023-01-10 19:29:05 +0300

    item adapted

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,8 @@
 $(function(){
     $('.item__carousel').slick({
-        prevArrow: "<img src='/img/previous-svgrepo-com.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='/img/next-svgrepo-com.svg' class='next' alt='2'>"
+        prevArrow: "<img src='/img/angle-left-svgrepo-com.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='/img/angle-right-svgrepo-com.svg' class='next' alt='2'>",
+        
     })
 
     $('.slct').click(function(){
